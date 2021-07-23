b895c0a06854719a611a1b216a966a014f7bfcda
Maria Morra <maria.morra@dedalus.eu>
2021-07-23 09:59:55 +0200

    added to lez_2/app2 and to lez2_appunti

--- lez_2/app2.js
+++ lez_2/app2.js
@@ -16,20 +16,28 @@ console.log(myVar);
 a();
 console.log(myVar);
 
+console.log("------------------------------------------------------------------------------");
+
 
 //same context functions
 function b1(){
     console.log(myVar);
+    //console.log(myVar1); ->causes error, b cannot see myVar1
 }
 
 function a1(){
     myVar=2;
     console.log(myVar);
+    var myVar1 = "hi"; // can i see it in b env? NO! a is b's brother, not it's parent!!!! both outher references of theese contexes go on the global level
+        // BUT i CAN SEE IT IN THE FOLLOWING FUNCTION !!! BECAUSE I AM THE LEXICAL CONTEXT OF THAT FUNCTION (BECAUSE I, A, DEFINED IT) 
+    var myPersonalFunction= function (){
+        console.log(myVar1);
+    }
+    myPersonalFunction(); 
     b1();
 }
 
 //calling wt same context
-var myVar1 = 1;
 console.log(myVar);
 a1();
 console.log(myVar);

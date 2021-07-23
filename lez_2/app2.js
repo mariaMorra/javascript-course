46e487990336a9c0847f5b1c569c5b98555f5e8e
Maria Morra <maria.morra@dedalus.eu>
2021-07-23 10:30:18 +0200

    fine chiacchera su scopes

--- lez_2/app2.js
+++ lez_2/app2.js
@@ -26,18 +26,23 @@ function b1(){
 }
 
 function a1(){
+    function myPersonalFunction(){
+        console.log(myVar1);
+        console.log(usedByMyPersonalFunction);
+        console.log(myVar);
+    }
     myVar=2;
     console.log(myVar);
     var myVar1 = "hi"; // can i see it in b env? NO! a is b's brother, not it's parent!!!! both outher references of theese contexes go on the global level
-        // BUT i CAN SEE IT IN THE FOLLOWING FUNCTION !!! BECAUSE I AM THE LEXICAL CONTEXT OF THAT FUNCTION (BECAUSE I, A, DEFINED IT) 
-    var myPersonalFunction= function (){
-        console.log(myVar1);
-    }
+        // BUT i CAN SEE IT IN THE MYPERSONAFUNCTION FUNCTION !!! BECAUSE I AM THE LEXICAL CONTEXT OF THAT FUNCTION (BECAUSE I, A, DEFINED IT) 
+    
     myPersonalFunction(); 
     b1();
 }
 
+var usedByMyPersonalFunction = "i was not defined in a, who defined myPersonalFunction BUT it CAN see me anyway bc global is still one of it's ancestors (and i was defined before the call to a)";
 //calling wt same context
 console.log(myVar);
 a1();
 console.log(myVar);
+
